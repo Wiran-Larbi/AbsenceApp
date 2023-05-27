e7e55d7b575c624428cb2faff89684dd43c0187b
Wiran_Larbi <71338128+Wiran-Larbi@users.noreply.github.com>
2023-05-27 19:03:51 +0100

    Initial commit

---
+++ src/main/java/com/ensah/absence/AbsenceAppApplication.java
@@ -0,0 +1,13 @@
+package com.ensah.absence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceAppApplication.class, args);
+	}
+
+}
